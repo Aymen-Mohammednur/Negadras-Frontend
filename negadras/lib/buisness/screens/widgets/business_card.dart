2a6b6254acbb821b05b0d34ecc091e5e74564f9f
Semere Habtu <se.semere.habtu@gmail.com>
2021-08-26 18:31:26 +0300

    Business card widget

---
+++ negadras/lib/buisness/screens/widgets/business_card.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:negadras/buisness/screens/widgets/star_rating.dart';
+
+class BusinessCard extends StatefulWidget {
+  final String businessName;
+  final String locationInfo;
+  BusinessCard({
+    Key? key,
+    required this.businessName,
+    required this.locationInfo,
+  }) : super(key: key);
+
+  @override
+  State<BusinessCard> createState() => _BusinessCardState();
+}
+
+class _BusinessCardState extends State<BusinessCard> {
+  double rating = 3.5;
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 170,
+      child: Card(
+        elevation: 10,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.businessName,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  StarRating(
+                    color: Colors.blue,
+                    rating: 3.5,
+                    onRatingChanged: (rating) =>
+                        setState(() => this.rating = rating),
+                  ),
+                  Text(widget.locationInfo)
+                ],
+              ),
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                isFavorite = !isFavorite;
+                setState(() {});
+              },
+              child: isFavorite
+                  ? Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    )
+                  : Icon(Icons.favorite_border_outlined),
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/hacker.jpeg'),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                    boxShadow: [
+                      BoxShadow(blurRadius: 7.0, color: Colors.black)
+                    ]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
