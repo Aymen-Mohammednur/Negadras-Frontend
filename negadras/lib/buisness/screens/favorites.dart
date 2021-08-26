d90d3715db50526414a4e7f4bb8af3efa7fdf002
Semere Habtu <se.semere.habtu@gmail.com>
2021-08-26 18:30:41 +0300

    Favorites screen

---
+++ negadras/lib/buisness/screens/favorites.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:negadras/buisness/screens/widgets/business_card.dart';
+
+class Favorites extends StatelessWidget {
+  const Favorites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+              child: const Text(
+                "Favorites",
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, i) {
+                  return BusinessCard(
+                    businessName: 'Kaldi\'s Coffee',
+                    locationInfo: 'Bole',
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
