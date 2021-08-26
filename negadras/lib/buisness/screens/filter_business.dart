05d3fc4a6105a931beaaa579a6c137101313f3de
Semere Habtu <se.semere.habtu@gmail.com>
2021-08-26 18:30:58 +0300

    Filter Business Screen

--- negadras/lib/buisness/screens/filter_business.dart
+++ negadras/lib/buisness/screens/filter_business.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:negadras/buisness/screens/widgets/business_card.dart';
+import 'package:negadras/buisness/screens/widgets/search_bar.dart';
 
 class FilterBusiness extends StatelessWidget {
   const FilterBusiness({Key? key}) : super(key: key);
@@ -8,74 +10,23 @@ class FilterBusiness extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 40,
-              width: double.infinity,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * .85,
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Enter a search term'),
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.search)
-                ],
+            const SearchBar(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+              child: const Text(
+                "Restaurants near you",
               ),
             ),
             Expanded(
               child: ListView.builder(
                 itemCount: 10,
                 itemBuilder: (context, i) {
-                  return Container(
-                    height: 170,
-                    child: Card(
-                      elevation: 10,
-                      child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Business Name"),
-                                Text("Ratings"),
-                                Text("Location Information")
-                              ],
-                            ),
-                          ),
-                          Spacer(),
-                          Icon(Icons.favorite_border_outlined),
-                          Spacer(),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: 100.0,
-                              height: 100.0,
-                              decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  image: DecorationImage(
-                                      image: AssetImage(
-                                          'assets/images/hacker.jpeg'),
-                                      fit: BoxFit.cover),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(75.0)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        blurRadius: 7.0, color: Colors.black)
-                                  ]),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  return BusinessCard(
+                    businessName: 'Kaldi\'s Coffee',
+                    locationInfo: 'Bole',
                   );
                 },
               ),
