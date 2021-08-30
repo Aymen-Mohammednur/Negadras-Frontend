4363e2477a237d91ca0c47ff67da6f0a6cd79989
semere01 <se.semere.tereffe@gmail.com>
2021-08-30 16:28:53 +0300

    add org-list page

---
+++ negadras/lib/buisness/screens/add_to_org.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:negadras/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'My Organizations'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Row singleOrgEntry(int i) {
+    return Row(children: [
+      Expanded(
+        child: Text(
+          "Organization $i",
+          style: normalText(),
+        ),
+      ),
+      Icon(Icons.delete),
+      Icon(Icons.edit)
+    ]);
+  }
+
+  ListView getLV() {
+    return ListView.builder(itemBuilder: (context, i) {
+      return Container(
+          height: 40,
+          decoration: listItemDecoration(),
+          child: singleOrgEntry(i),
+          padding: EdgeInsets.only(left: 15, right: 15),
+          margin: EdgeInsets.only(bottom: 5, top: 5, left: 13, right: 13));
+    });
+  }
+
+  Widget switchButton =
+      ElevatedButton(onPressed: () {}, child: Text("My Businesses"));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            switchButton,
+            Container(child: getLV(), width: 400, height: 700),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
