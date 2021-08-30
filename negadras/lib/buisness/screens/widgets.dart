ff5d3725331f949751d85bd79158ed55a2f4a5f9
semere01 <se.semere.tereffe@gmail.com>
2021-08-30 09:24:21 +0300

    add imageStackWidget for business headers

--- negadras/lib/buisness/screens/widgets.dart
+++ negadras/lib/buisness/screens/widgets.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:negadras/SimpleBarChart.dart';
 
+
+TextStyle normalText() {
+  return TextStyle(
+    fontSize: 24,
+  );
+}
+
 Widget searchBar() {
   return Row(
     children: [
@@ -15,6 +22,7 @@ Widget searchBar() {
     ],
   );
 }
+
 Widget chartWidget(){
   return SizedBox(child: 
   SimpleBarChart.withSampleData(),
@@ -22,3 +30,31 @@ Widget chartWidget(){
   );
 }
 
+Widget imageStackWidget() {
+  return Stack(
+    alignment: Alignment(0, 0.85),
+    children: [
+      Image.asset(
+        "images/placeholder-1.jpg",
+        height: 220,
+        fit: BoxFit.cover,
+        
+        ),
+      Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Business Name",
+              style: normalText(),
+              ),
+            Text(
+              "N⭐",
+              style: normalText(),),
+          ],
+        ),
+      ),
+    ],
+  );
+}
