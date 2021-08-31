73d2e767968b78218213e70fc65a17e91f0a48d2
Aymen Moh <aymenmohammednur12@gmail.com>
2021-08-31 14:34:15 +0300

    Added drop business section in the edit business screen

--- negadras/lib/buisness/screens/widgets/edit_business.dart
+++ negadras/lib/buisness/screens/widgets/edit_business.dart
@@ -24,8 +24,8 @@ class Home extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: [
-            BusinessPhoto(),
-            // DropBusiness(),
+            // BusinessPhoto(),
+            DropBusiness(),
             // EditForm(),
           ],
         ),
@@ -56,4 +56,52 @@ class BusinessPhoto extends StatelessWidget {
   }
 }
 
+class DropBusiness extends StatelessWidget {
+  const DropBusiness({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Organization Name"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete),
+                    iconSize: 50,
+                  ),
+                  Text("Drop Business"),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bar_chart),
+                    iconSize: 50,
+                  ),
+                  Text("Statistics"),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.reviews),
+                    iconSize: 50,
+                  ),
+                  Text("Reviews"),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
