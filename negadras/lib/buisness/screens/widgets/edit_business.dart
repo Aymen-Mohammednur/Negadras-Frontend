a7abac837bd65f52e9ff89d9a2cf9b17e2b0689d
Aymen Moh <aymenmohammednur12@gmail.com>
2021-08-31 14:36:44 +0300

    Edit form section added

--- negadras/lib/buisness/screens/widgets/edit_business.dart
+++ negadras/lib/buisness/screens/widgets/edit_business.dart
@@ -105,3 +105,169 @@ class DropBusiness extends StatelessWidget {
     );
   }
 }
+
+class EditForm extends StatelessWidget {
+  late String name;
+  late String type;
+  late String location;
+  late String website;
+  late String phoneNumber;
+  late String email;
+  late String organization;
+
+  final List<String> types = ['Restaurant', 'Hotel', 'Shop', 'Cafe', 'Other'];
+  String _currentType = "Choose";
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Name"),
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          return 'Name is required';
+        }
+      },
+      onSaved: (String? value) {
+        name = value!;
+      },
+    );
+  }
+
+  Widget _buildType() {
+    // return TextFormField(
+    //   decoration: InputDecoration(labelText: "Type"),
+    //   // validator: (String? value) {
+    //   //   if (value!.isEmpty) {
+    //   //     return 'Password is required';
+    //   //   }
+    //   // },
+    //   // validator: (val) => val!.isEmpty ? 'Type is required' : null,
+    //   onSaved: (String? value) {
+    //     type = value!;
+    //   },
+    // );
+
+    return DropdownButton(
+      // value: _currentType,
+
+      // items: types.map((type) {
+      //   return DropdownMenuItem(
+      //     value: type,
+      //     child: Text("$type"),
+      //   );
+      // }).toList(),
+      items: [
+        DropdownMenuItem(
+          child: Text("Hi"),
+        ),
+        DropdownMenuItem(child: Text("Wow"))
+      ],
+      // onSaved: (String? value) {
+      //   _currentType = value!;
+      // },
+    );
+  }
+
+  Widget _buildLocation() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Location"),
+      onSaved: (String? value) {
+        location = value!;
+      },
+    );
+  }
+
+  Widget _buildWebsite() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Website"),
+      keyboardType: TextInputType.url,
+      onSaved: (String? value) {
+        website = value!;
+      },
+    );
+  }
+
+  Widget _buildPhoneNumber() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Phone Number"),
+      keyboardType: TextInputType.phone,
+      onSaved: (String? value) {
+        phoneNumber = value!;
+      },
+    );
+  }
+
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Email"),
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          return 'Name is required';
+        }
+
+        if (!RegExp(
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            .hasMatch(value)) {
+          return 'Please enter a valid email address';
+        }
+      },
+      onSaved: (String? value) {
+        email = value!;
+      },
+    );
+  }
+
+  Widget _buildOrganization() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: "Organization"),
+      onSaved: (String? value) {
+        organization = value!;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(24),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildName(),
+            SizedBox(height: 20),
+            _buildType(),
+            // _buildLocation(),
+            // _buildWebsite(),
+            // _buildPhoneNumber(),
+            // _buildEmail(),
+            // _buildOrganization(),
+            SizedBox(height: 100),
+            ElevatedButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) {
+                  return;
+                }
+
+                _formKey.currentState!.save();
+
+                print(name);
+                // print(type);
+                // print(location);
+                // print(website);
+                // print(phoneNumber);
+                // print(email);
+              },
+              child: Text(
+                "Update",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
