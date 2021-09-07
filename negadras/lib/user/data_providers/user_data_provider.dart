12e131b8c98fda71b2a124636f5fe5e363f933c1
betziii <se.bethelhem.teshibelay@gmail.com>
2021-09-07 14:46:54 +0300

    edit on update for user

--- negadras/lib/user/data_providers/user_data_provider.dart
+++ negadras/lib/user/data_providers/user_data_provider.dart
@@ -14,28 +14,42 @@ class UserDataProvider {
       throw Exception("Failed to get user information");
     }
   }
+
   Future<User> update(String id, User user) async {
-    final response = await http.put(Uri.parse("$_baseUrl/$id"),
+    final response = await http.patch(Uri.parse("$_baseUrl/$id"),
         headers: <String, String>{"Content-Type": "application/json"},
         body: jsonEncode({
           // "firstName": user.firstName,
           // "lastName": user.lastName,
           "username": user.username,
-          "password": user.password,
           // "role": user.role,
         }));
 
     if (response.statusCode == 200) {
       return User.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception("Failed to update username or password");
+      throw Exception("Failed to update username");
     }
   }
+
+  Future<User> change(String id, User user) async {
+    final response = await http.patch(Uri.parse("$_baseUrl/$id"),
+        headers: <String, String>{"Content-Type": "application/json"},
+        body: jsonEncode({
+          "password": user.password,
+        }));
+
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to update password");
+    }
+  }
+
   Future<void> delete(String id) async {
     final response = await http.delete(Uri.parse("$_baseUrl/$id"));
     if (response.statusCode != 204) {
       throw Exception("Field to delete the user");
     }
   }
-
 }
