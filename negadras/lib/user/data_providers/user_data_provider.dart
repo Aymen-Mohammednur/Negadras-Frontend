8c938996d0998a8bb0b5b1922e7c13b721e59a44
betziii <se.bethelhem.teshibelay@gmail.com>
2021-09-07 14:36:31 +0300

    data provider update for user

--- negadras/lib/user/data_providers/user_data_provider.dart
+++ negadras/lib/user/data_providers/user_data_provider.dart
@@ -14,4 +14,22 @@ class UserDataProvider {
       throw Exception("Failed to get user information");
     }
   }
+  Future<User> update(String id, User user) async {
+    final response = await http.put(Uri.parse("$_baseUrl/$id"),
+        headers: <String, String>{"Content-Type": "application/json"},
+        body: jsonEncode({
+          // "firstName": user.firstName,
+          // "lastName": user.lastName,
+          "username": user.username,
+          "password": user.password,
+          // "role": user.role,
+        }));
+
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to update username or password");
+    }
+  }
+
 }
